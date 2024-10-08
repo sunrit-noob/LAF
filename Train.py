18a6e74d57a5ccb5cf76c217bd587dfd7c6e7414
sunrit-noob <136549077+sunrit-noob@users.noreply.github.com>
2024-10-08 07:06:57 +0530

    Update Train.py

--- Train.py
+++ Train.py
@@ -110,6 +110,82 @@ def test(trained_model, test_loader, output_label = None):
 '''
 x_out is the output image data, and x is the original image data
 '''
+def testHypothesis(trained_model, retrained_model, test_loader, output_label = None, test_label = 0, labelAgnostic=False):
+    trained_model = trained_model.cuda()
+    retrained_model = retrained_model.cuda()
+    trained_model.eval()
+    retrained_model.eval()
+    all_preds = torch.tensor([])
+    targets = torch.tensor([])
+    original_targets = torch.tensor([])
+    matchLength = torch.tensor([])
+    total = 0
+    correct = 0
+    with torch.no_grad():
+
+        for i, (x,y) in enumerate(test_loader):
+            
+            original_targets = torch.cat((original_targets, y.cpu()),dim=0)
+                            
+            x = x.cuda()
+            y = y.type(torch.LongTensor)
+            y = y.cuda()
+            y_out, _ = trained_model(x)
+            y_out_retrained, _ = retrained_model(x)
+
+            predicted_set_original = torch.from_numpy(np.argsort(-y_out.numpy()), dtype=torch.float32)
+            predicted_set_retrained = torch.from_numpy(np.argsort(-y_out_retrained.numpy()), dtype=torch.float32)
+            if(labelAgnostic):
+                newPredictedSet = predicted_set_original[1:]
+                ind = torch.where(predicted_set_retrained == predicted_set_original[0])
+                newRetrainedSet = torch.cat(predicted_set_retrained[:ind], predicted_set_retrained[ind+1:])
+            else:
+                indOrg = torch.where(predicted_set_retrained == test_label)
+                newPredictedSet = torch.cat(predicted_set_original[:indOrg], predicted_set_original[indOrg+1:])
+                indRet = torch.where(predicted_set_retrained == test_label)
+                newRetrainedSet = torch.cat(predicted_set_retrained[:indRet], predicted_set_retrained[indRet+1:])
+
+            for i in range(1, len(newPredictedSet)):
+                if(newPredictedSet[:i] != newRetrainedSet[:i]):
+                    y1 = torch.tensor([i])
+                    matchLength = torch.cat((matchLength, y1.cpu()),dim=0)
+                
+            _, predicted = torch.max(y_out.data, dim=1)
+            all_preds = torch.cat((all_preds, y_out.cpu()),dim=0)
+            targets = torch.cat((targets, y.cpu()),dim=0)
+            
+            total += y.size(0)
+            correct += (predicted == y).sum().item()   
+
+        preds = all_preds.argmax(dim=1)
+        targets = np.array(targets)
+        acc = 100.0 * correct / total
+        #print('The accuracy of the all classes is: ', acc)
+        if output_label is None:
+            #print('Confusion matrix and classification report of all classes are: ')
+            #print(confusion_matrix(targets, preds))
+            #print(classification_report(targets, preds))
+            selected_acc = acc
+        else:
+            selected_preds = []
+            selected_targets = []
+            selected_total = 0
+            for i in range(len(preds)):
+                if original_targets[i] in output_label:
+                    selected_preds.append(preds[i])
+                    selected_targets.append(targets[i])
+                    selected_total += 1  
+            # if len(output_label) == 1:        
+            #     print(selected_preds) 
+                        
+            selected_correct = (np.array(selected_preds) == np.array(selected_targets)).sum().item()                  
+            selected_acc = 100.0 * selected_correct / selected_total
+            #print('The accuracy of the selected classes ', output_label, 'is: ', selected_acc)
+
+    return selected_acc, matchLength
+
+
+
 def vae_loss_function(x_out, x, mu, sigma):
     #print(x_out.shape)
     BCE = F.mse_loss(x_out, x, reduction='sum')/x.shape[0]
